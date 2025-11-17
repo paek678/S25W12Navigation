06cd368ccf46e526baa29b20549ceb061d1f8e37
백성열 <paek678@naver.com>
2025-11-16 23:37:41 -0800

    Initial Commit

---
+++ S25W12Navigation/S25W12NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W12NavigationApp.swift
+//  S25W12Navigation
+//
+//  Created by Student on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W12NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
